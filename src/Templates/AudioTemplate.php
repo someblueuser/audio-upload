864a7c8d0b1f8bcbeadafd5107d5c9ffd826b3a3
Slawomir Pawicki <slawomir.pawicki@gmail.com>
2024-06-07 14:32:50 +0200

    additional mime types

--- src/Templates/AudioTemplate.php
+++ src/Templates/AudioTemplate.php
@@ -7,6 +7,15 @@ use FoF\Upload\File;
 
 class AudioTemplate implements Template
 {
+    protected $mimeToShortcode = [
+        'audio/mpeg' => 'mp3',
+        'audio/ogg' => 'ogg',
+        'audio/x-m4a' => 'm4a',
+        'audio/wav' => 'wav',
+        'audio/flac' => 'flac',
+        'audio/webm' => 'webm'
+    ];
+
     public function tag(): string
     {
         return 'audioUploadTemplate';
@@ -24,6 +33,8 @@ class AudioTemplate implements Template
 
     public function preview(File $file): string
     {
-        return '[audio mp3="'. $file->url .'"]';
+        $mime = $file->mimeType;
+        $extension = $this->mimeToShortcode[$mime] ?? 'mp3'; // Default to mp3 if mime type is not in the list
+        return '[audio '. $extension .'="'. $file->url .'"]';
     }
 }
